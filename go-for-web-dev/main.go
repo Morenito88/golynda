819b0241102b0a457cdd7f38e0b54f966a138268
Hugo Santos <hsantos@leya.com>
2016-12-03 02:06:10 +0000

    search books from external api

--- go-for-web-dev/main.go
+++ go-for-web-dev/main.go
@@ -4,8 +4,14 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"net/url"
 
 	"database/sql"
+
+	"encoding/json"
+	"encoding/xml"
+
+	"io/ioutil"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -13,6 +19,17 @@ import (
 type Page struct {
 	Name     string
 	DBStatus bool
+}
+
+type SearchResult struct {
+	Title  string `xml:"title,attr"`
+	Author string `xml:"author,attr"`
+	Year   string `xml:"hyr,attr"`
+	ID     string `xml:"owi,attr"`
+}
+
+type ClassifySearchResponse struct {
+	Results []SearchResult `xml:"works>work"`
 }
 
 func main() {
@@ -35,5 +52,39 @@ func main() {
 		}
 	})
 
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		var results []SearchResult
+		var err error
+
+		if results, err = search(r.FormValue("search")); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		encoder := json.NewEncoder(w)
+		if err := encoder.Encode(results); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
 	fmt.Println(http.ListenAndServe("localhost:8080", nil))
+}
+
+func search(query string) ([]SearchResult, error) {
+	var resp *http.Response
+	var err error
+
+	if resp, err = http.Get("http://classify.oclc.org/classify2/Classify?&summary=true&title=" + url.QueryEscape(query)); err != nil {
+		return []SearchResult{}, err
+	}
+
+	defer resp.Body.Close()
+	var body []byte
+	if body, err = ioutil.ReadAll(resp.Body); err != nil {
+		return []SearchResult{}, err
+	}
+
+	var c ClassifySearchResponse
+	err = xml.Unmarshal(body, &c)
+
+	return c.Results, err
 }
